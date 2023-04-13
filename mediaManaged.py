fd5f66af3f8a78a5166ffdc4163070b39611abde
kylejamesbyron <markandrewisom@gmail.com>
2023-04-13 03:36:16 -0400

    working

--- mediaManaged.py
+++ mediaManaged.py
@@ -73,4 +73,9 @@ for dirname in os.listdir(userdir):
 	print("------------")
 	photodir = ("users/" + dirname)
 	for photo in os.listdir(photodir):
+		record = ("home/maisom/Documents/GitHub/mediaManager/users/" + dirname + "/" + photo)
+		print(record)
+		cursor.execute("INSERT OR IGNORE INTO photos (name, link) values (?, ?)", [dirname, record])
 		print(photo)
+
+connection.commit() 
